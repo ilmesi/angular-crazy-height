f613cb51763bad47677f1b5e87fd997ad702b3ea
ilmesi <nachoguason@gmail.com>
2016-02-19 20:02:38 +0100

    jqlite & small fixes

--- index.js
+++ index.js
@@ -3,17 +3,17 @@
 
   angular
     .module('crazy-resize', [])
-    .directive('resize-height', resizeHeight);
+    .directive('resizeHeight', resizeHeight);
 
   function resizeHeight($window) {
 
     return function($scope, $element, attributes) {
       // Only percentage for now
       var winElement = angular.element($window);
-      var resizeValue = attributes['resize-height'];
+      var resizeValue = attributes['resizeHeight'];
 
       var changeHeight = function(event) {
-        var currentHeight = winElement.height() * parseFloat(resizeValue);
+        var currentHeight = winElement[0].innerHeight * parseFloat(resizeValue);
         $element.css('height', currentHeight + 'px');
       };
 
