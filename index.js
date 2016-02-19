0bcfce7747277cab924bfdce2db1a9f1632746f7
ilmesi <nachoguason@gmail.com>
2016-02-19 14:20:35 +0100

    wip percentage

---
+++ index.js
@@ -0,0 +1,26 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('findtrip')
+    .directive('resize-height', resizeHeight);
+
+  function resizeHeight($window) {
+
+    return function($scope, $element, attributes) {
+      // Only percentage for now
+      var winElement = angular.element($window);
+      var resizeValue = attributes['resize-height'];
+
+      var changeHeight = function(event) {
+        var currentHeight = winElement.height() * parseFloat(resizeValue);
+        $element.css('height', currentHeight + 'px');
+      };
+
+      winElement.bind('resize', changeHeight);
+      changeHeight(); // when page loads 
+    }
+
+  }
+
+})();
